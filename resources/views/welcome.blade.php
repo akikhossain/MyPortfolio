6fbdf05a431244351ac504e2fa888bca37737c3d
akik-hossain <akikhs00@gmail.com>
2024-02-02 01:46:56 +0600

    worked with the header part

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,43 +8,51 @@
     {{--
     <link rel="stylesheet" href="{{ asset('css/app.css') }}"> --}}
     @vite('resources/css/app.css')
-    @vite('resources/css/portfolio.css')
     <title>Discover Akik Hossain</title>
+
 </head>
 
 <body>
-    <header class="bg-orange-100">
-        <div>
-
+    <header class="bg-orange-50">
+        <div class="flex justify-start items-start">
+            <div class="absolute ml-6 mt-1">
+                <img src="{{ asset('assests/images/developer.png') }}" alt="">
+            </div>
+            <nav class="flex justify-between container mx-auto items-center">
+                <div class="font-bold text-5xl mt-10">
+                    {{-- <h3><a href=""></a>AK<span class="text-orange-500">I</span>K</h3> --}}
+                    <img class="w-25 h-14" src="{{ asset('assests/images/4.png') }}" alt="">
+                </div>
+                <div class="mt-10">
+                    <ul class="flex gap-10 text-xl">
+                        <li><a href="">Portfolio</a></li>
+                        <li><a href="">Blog</a></li>
+                        <li><a role="button"
+                                class="bg-orange-600 hover:bg-transparent hover:text-orange-600 font-bold text-white px-4 py-2 border rounded">
+                                Hire Me
+                            </a></li>
+                    </ul>
+                </div>
+            </nav>
         </div>
-        <nav class="flex justify-between container mx-auto items-center">
-            <div class="font-bold text-5xl mt-10">
-                <h3><a href=""></a>AK<span class="text-orange-500">I</span>K</h3>
-            </div>
-            <div class="mt-10">
-                <ul class="flex gap-10 text-xl">
-                    <li><a href="">Portfolio</a></li>
-                    <li><a href="">Blog</a></li>
-                    <li><a role="button" class="bg-orange-600 hover:bg-red-700 text-white px-4 py-2 border rounded">
-                            Hire Me
-                        </a></li>
-                </ul>
-            </div>
-        </nav>
-        <div class="flex justify-between container mx-auto items-center">
+        <div class="flex justify-between container mx-auto items-center mt-20">
             <div>
-                <h3 class="text-5xl mb-2">Hi, I am</h3>
-                <h1 class="font-bold text-8xl mb-2">Akik Hossain</h1>
-                <p class="mb-2">
+                <h3 class="text-5xl mb-3">Hi, I am</h3>
+                <h1 class="font-bold text-8xl mb-4">Akik Hossain</h1>
+                <p class="mb-3 text-xl">
                     Shot what able cold new see hold.Friendly as an betrayed formerly
                     <br />
                     he. Morning because as to society behaved moments.
                 </p>
                 <div class="mt-8">
-                    <a role="button" class="bg-orange-600 hover:bg-red-700 text-white text-xl px-8 py-4 border rounded">
+                    <a role="button"
+                        class="bg-orange-600 hover:bg-transparent hover:text-orange-600 font-bold text-white text-xl px-8 mr-2 py-4 border rounded"><i
+                            class="fa-solid fa-download mr-1"></i>
                         Download CV
                     </a>
-                    <a role="button" class="bg-orange-600 hover:bg-red-700 text-white text-xl px-8 py-4 border rounded">
+                    <a role="button"
+                        class="bg-orange-600 hover:bg-red-700 hover:bg-transparent hover:text-orange-600 font-bold text-white text-xl px-8 py-4 border rounded"><i
+                            class="fa-solid fa-phone mr-1"></i>
                         Contact
                     </a>
                 </div>
@@ -57,7 +65,7 @@
         </div>
     </header>
 
-
+    <script src="https://kit.fontawesome.com/5c95e5cc68.js" crossorigin="anonymous"></script>
 </body>
 
 </html>
