a06825aaac2d1aa0a6cadea32e536175b3982627
akik-hossain <akikhs00@gmail.com>
2024-02-01 17:48:32 +0600

    added tailwind css as a framework

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    {{--
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}"> --}}
+    @vite('resources/css/app.css')
+    <title>Discover Akik Hossain</title>
+</head>
+
+<body>
+    <h1 class="text-center font-bold text-5xl bg-orange-400 p-3 box-border container mx-auto uppercase mt-10">hello i am
+        akik
+        hossain</h1>
+
+</body>
+
+</html>
